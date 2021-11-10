8454393efc9320b0a48d8f4b828e374b4babfa8b
Adilene Bejines <adi3511@hotmail.com>
2021-11-09 18:37:45 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,8 +72,14 @@ var fight = function(enemyName) {
         }
         }
     };
+
 //function to start a new game
 var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
     //fight each enemy-robot by looping over them and fighting them one at a time
     for(var i = 0; i < enemyNames.length; i++) {
         //if player is still alive, keep fighting
@@ -96,5 +102,26 @@ var startGame = function() {
             break;
         }
     }
+    //play again
+    endGame();
 };
+    var endGame = function() {
+        //if player is stil alive, player wins!
+        if (playerHealth > 0) {
+            window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ". ");
+        } else {
+            window.alert("The game has now ended. Let's see how you did!");
+        }
+        
+    //asl the player if they'd like to play again
+    var playAgainConfirm = window.confirm("would you like to play again?");
+
+        if (playAgainConfirm) {
+            //restart the game
+            startGame();
+        } else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
 startGame();
